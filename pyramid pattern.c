b2004eda45a3d3fcdd1ebdf4f1609a2690eba4c7
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2023-12-19 19:11:04 +0530

    Add files via upload

---
+++ pyramid pattern.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("enter the number of rows you want to print?");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n+i-1;j++)
+        {
+            if(j<=n-i)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=2*n-i;j++)
+        {
+            if(j<=i-1)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+}
+
