c605f258311ea24fe99cf54b9a21a43418ef77a7
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2024-01-02 22:20:05 +0530

    Add files via upload

---
+++ write a program to print any table.c
@@ -0,0 +1,22 @@
+ #include<stdio.h>
+ void main()
+ {
+     int a[100];
+     int i,n;
+     printf("enter a number");
+     scanf("%d",&n);
+      for(i=1;i<=10;i++)
+    {
+        printf("%d*%d=%d\n",n,i,n*i);
+    }
+
+
+ }
+
+
+
+
+
+
+
+
