c83301a78cade6eb846de7997d0a6fe64055bfda
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2024-01-02 22:26:01 +0530

    Add files via upload

---
+++ write a program to print english alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int i=65;
+    printf(" capital letters a to z\n");
+    for(i=65;i<=90;i++)
+    {
+        printf("\t%c",i);
+    }
+    printf("\n small letters A to Z\n ");
+    int j=97;
+    for(j=97;j<=122;j++)
+    {
+        printf("\t%c",j);
+    }
+}
