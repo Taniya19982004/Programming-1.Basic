fe4000714fe31102ce2da6ad0f040bea2e5a30cf
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2024-01-04 23:51:30 +0530

    Add files via upload

---
+++ write a program of average of three number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ void main()
+
+ {
+     int a,b,c,avg;
+     printf("Enter three number");
+     scanf("%d%d%d",&a,&b,&c);
+     avg=a+b+c/3.0;
+     printf("average of three number is %f",avg);
+
+ }
