09d8daac46795b9dc34ba78f71d9f8997b880664
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2024-01-09 23:21:46 +0530

    Add files via upload

---
+++ convert a temperature from celcius to temperature.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     float c=63.5,f;
+     cout<<"Enter a temperature in celcius-->";
+     cin>>c;
+     f=(c*9/5)+32;
+     cout<<"temeratue in farenheit  "<<f;
+return 0;
+}
