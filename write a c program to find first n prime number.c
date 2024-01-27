5c22722453d0d4aa1c832d21993a2f314ed7efb7
Taniya19982004 <154086223+Taniya19982004@users.noreply.github.com>
2024-01-27 23:57:54 +0530

    Add files via upload

---
+++ write a c program to find first n prime number.c
@@ -0,0 +1,11 @@
+//TO FIND FIRST N PRIME NUMBER
+#include<stdiio.>
+void main()
+{
+    int n,i,j;
+    printf("Enter a number that will help to find how many prime number are");
+    scanf("%d",&n);
+    while(n)
+    {
+        if()
+    }
